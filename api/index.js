8c40283f8ce2aa6a09bae86f451f8bf5fbab9e34
lolito <prof.lolito@gmail.com>
2023-11-11 17:29:30 +0200

    Logout Start

--- api/index.js
+++ api/index.js
@@ -6,13 +6,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
-
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret =  'ujk857y383ifnkmlertert6357';
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://radlee:Leander247365@mern-blog.psjtrcb.mongodb.net/?retryWrites=true&w=majority');
 
@@ -32,11 +33,31 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const {username, password} = req.body;
-    const user = await User.findOne({username})
-    const passOk = bcrypt.compareSync(password, user.password)
-    res.json(passOk)
+    const userDoc = await User.findOne({username})
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+    if(passOk) {
+        //logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('OK');
+        })
+    } else {
+        res.status(400).json('Wrong Shit Bro');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if(err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('Ok');
 })
 
-var listener = app.listen(4000, function(){
-    console.log('\nListening on port ' + listener.address().port );
+var listener = app.listen(4000, () => {
+    console.log('\nServer running on PORT: ' + listener.address().port );
 });
