136c56297557dbfb13be61442d8622be8a04ebb3
lolito <prof.lolito@gmail.com>
2023-11-11 17:56:56 +0200

    UserContext

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header () {
-  const [username, setUsername] = useState(null);
-
+  const {setUserInfo, userInfo} = useContext(UserContext);
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
-        setUsername(userInfo.username);
+        setUserInfo(userInfo);
       });
     });
   }, []);
@@ -18,8 +18,11 @@ export default function Header () {
     fetch('http://localhost:4000/logout', {
       credentials: 'include',
       method: 'POST'
-    })
+    });
+    setUserInfo(null);
   }
+
+  const username = userInfo?.username;
     return(
         <header>
         <Link to="/" className="logo">radBlok</Link>
