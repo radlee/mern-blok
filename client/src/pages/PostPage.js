951c1398dbb7d3a0ea5a4f3946156a8fb5b29c0a
lolito <prof.lolito@gmail.com>
2023-11-12 16:47:15 +0200

    Single Post State Set

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,8 +1,16 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
 
 export default function PostPage() {
+    const [postInfo, setPostInfo] = useState(null);
+    const {id} = useParams();
     useEffect(() => {
-        
+        fetch(`http://localhost:4000/post/${id}`)
+        .then(response => {
+            response.json().then(postInfo => {
+                setPostInfo(postInfo);
+            });
+        });
     }, []);
     return (
         <div>
